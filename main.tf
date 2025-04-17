5b6babccadafb88934cd4fd274b43028d580a397
krishna <krishna2025@gmail.com>
2025-04-17 11:33:38 +0530

    update3

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "client1_vpc" {
 }
 
 module "client1_subnet" {
-    source = "./module/aws-subnet"
+    source = "./module/aws_subnet"
     vpc_id = module.client1_vpc.vpc_id
     cidr_block = var.client1_subnet_cidr_block
     availability_zone = var.client1_subnet_az
